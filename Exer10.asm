e23e5edb54e3dc2030b40a2780a778d2c7198df5
etawn <univaltacc6969@gmail.com>
2025-08-23 20:32:09 +0800

    10 and 11 DONE

--- Exer10.asm
+++ Exer10.asm
@@ -1,3 +1,7 @@
+; Filename: Exer10.asm
+; Displaying a duck with blinking eyes
+; Name: Ethan Benedict M. Labajo
+; Date: August 22, 2025
 .model small
 .stack 100
 
@@ -191,13 +195,39 @@ start:
     int 21h
 
     mov bl, 6Eh
-    mov cx, 10d
+    mov cx, 2d
     int 10h
 
-    mov dx, offset px3
+    mov dx, offset ts
+    int 21h
+    
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
     int 21h
 
-    ; ROW 4
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    ; WE PUT BLINKING HERE
+
 
     mov dx, offset nl
     int 21h
@@ -378,6 +408,183 @@ start:
 
     mov dx, offset ts
     int 21h
+
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov dx, offset nl
+    int 21h
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    mov bl, 1Bh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov dx, offset nl
+    int 21h
+
+    ; ROW 7
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 1Bh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
     mov bl, 6eh
     mov cx, 2d
     int 10h
@@ -386,10 +593,470 @@ start:
     int 21h
 
 
+    mov bl, 1Bh
+    mov cx, 2d
+    int 10h
 
+    mov dx, offset ts
+    int 21h
 
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
 
+    mov dx, offset ts
+    int 21h
 
+    mov bl, 6eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov dx, offset nl
+    int 21h
+
+    ; ROW 8 
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 1Bh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 1Bh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 1Bh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov dx, offset nl
+    int 21h
+
+    ; ROW 9
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov dx, offset nl
+    int 21h
+
+    ; ROW 10
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov dx, offset nl
+    int 21h
+
+    ; ROW 11
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 00h
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov bl, 6Eh
+    mov cx, 2d
+    int 10h
+
+    mov dx, offset ts
+    int 21h
+
+    mov ah, 02h
+    mov bh, 0
+    mov dh, 15     ; row number
+    mov dl, 10     ; column number 
+    int 10h
+
+blink_loop:
+    ; --- draw left eye ---
+    mov ah, 02h
+    mov bh, 0
+    mov dh, 15       ; row
+    mov dl, 10       ; col
+    int 10h
+
+    mov ah, 09h
+    mov al, 219      ; █ full block
+    mov bl, 07h
+    mov bh, 0
+    mov cx, 1
+    int 10h
+
+    ; --- draw right eye ---
+    mov ah, 02h
+    mov bh, 0
+    mov dh, 15
+    mov dl, 11       ; col for second eye
+    int 10h
+
+    mov ah, 09h
+    mov al, 219
+    mov bl, 07h
+    mov bh, 0
+    mov cx, 1
+    int 10h
+
+    call delay
+
+    ; --- erase left eye ---
+    mov ah, 02h
+    mov bh, 0
+    mov dh, 15
+    mov dl, 10
+    int 10h
+
+    mov ah, 09h
+    mov al, ' '
+    mov bl, 07h
+    mov bh, 0
+    mov cx, 1
+    int 10h
+
+    ; --- erase right eye ---
+    mov ah, 02h
+    mov bh, 0
+    mov dh, 15
+    mov dl, 11
+    int 10h
+
+    mov ah, 09h
+    mov al, ' '
+    mov bl, 07h
+    mov bh, 0
+    mov cx, 1
+    int 10h
+
+    call delay
+    jmp blink_loop
+
+delay proc
+    mov ah, 00h
+    int 1Ah         
+    add dx, 100     
+
+wait_loop:
+    mov ah, 00h
+    int 1Ah
+    cmp dx, dx       
+    jb wait_loop
+    ret
+delay endp
 
     int 27h
 
